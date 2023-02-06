5ec1371c109d5cc0c98f9df27c8b4299e8e121a7
ilgarcia <60369262+ilgarcia@users.noreply.github.com>
2023-02-06 01:09:16 -0300

    Changing contact me section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,7 +6,7 @@ import { motion as m, Variants } from "framer-motion";
 import { Experience } from "../components/Experience";
 
 import graphics from "./img/graphics.png";
-// import contact from "./img/contact.png";
+import contact from "./img/contact.png";
 
 import styles from "./page.module.scss";
 import { Socials } from "../components/Socials";
@@ -85,64 +85,23 @@ export default function Page() {
           variants={cardVariants}
         >
           <h2 className={styles.numberedHeaders}>Where I've Worked</h2>
-          
+
           <Experience />
         </m.section>
-        {/* <section id="contact">
-        <div className={styles.containerContact}>
-          <Image
-            src={contact}
-            alt="Contact me"
-            className={styles.contactPic}
-            fill
-            sizes="100%"
-          />
-          <h2>CONTACT ME</h2>
-        </div>
-        <div className={styles.socialContainer}>
-          <div className={styles.socials}>
-            <a>
-              <RiGithubFill />
-              <div>
-                <h3>GITHUB</h3>
-                <p>Follow to see my projects</p>
-              </div>
-            </a>
-            <a>
-              <RiLinkedinBoxFill />
-              <div>
-                <h3>LINKEDIN</h3>
-                <p>Let's connect on linkedin</p>
-              </div>
-            </a>
-            <a>
-              <RiNewspaperLine />
-              <div>
-                <h3>RESUME</h3>
-                <p>Download my resume</p>
-              </div>
-            </a>
-          </div>
-          <div className={styles.messageContainer}>
-            <p>
-              Send a general message or details of a project you'd like me to be
-              a part of and I'll get back to you as soon as possible.
-            </p>
-            <form className={styles.messageForm}>
-              <div>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="E-mail" />
-              </div>
-              <div>
-                <textarea rows={8} placeholder="Message" />
-              </div>
-              <button type="submit">Send message!</button>
-            </form>
-          </div>
-        </div>
-      </section> */}
+        {/* <m.section id="contact"> */}
+          {/* <div className={styles.containerContact}>
+            <Image
+              src={contact}
+              alt="Contact me"
+              className={styles.contactPic}
+              fill
+              sizes="100%"
+            />
+            <h2>CONTACT ME</h2>
+          </div> */}
+          <Socials />
+        {/* </m.section> */}
       </div>
-      <Socials />
     </main>
   );
 }
--- client/src/components/Socials/index.tsx
+++ client/src/components/Socials/index.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import Image from "next/image";
 import { motion as m, Variants } from "framer-motion";
 
 import { FiGithub, FiLinkedin, FiFileText } from "react-icons/fi";
+
+import contact from "./img/contact.png";
 
 import styles from "./styles.module.scss";
 
@@ -17,55 +20,115 @@ const itemVariants: Variants = {
 
 export function Socials() {
   return (
-    <m.div
-      className={styles.socials}
-      initial="hidden"
-      animate="visible"
-      variants={itemVariants}
-    >
-      <ul className={styles.socialsList}>
-        <li>
-          <a
-            href="mailto:limagarcia.igor@gmail.com"
-            className={styles.emailDisplay}
-          >
-            limagarcia.igor@gmail.com
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/ilgarcia"
-            className={styles.svgDisplay}
-            aria-label="GitHub"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FiGithub />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/ilgarcia/"
-            className={styles.svgDisplay}
-            aria-label="LinkedIn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FiLinkedin />
-          </a>
-        </li>
-        <li>
-          <a
-            href="/resume.pdf"
-            className={styles.svgDisplay}
-            aria-label="Resume"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FiFileText />
-          </a>
-        </li>
-      </ul>
-    </m.div>
+    <div>
+      <m.div
+        className={styles.socials}
+        initial="hidden"
+        animate="visible"
+        variants={itemVariants}
+      >
+        <ul className={styles.socialsList}>
+          <li>
+            <a
+              href="mailto:limagarcia.igor@gmail.com"
+              className={styles.emailDisplay}
+            >
+              limagarcia.igor@gmail.com
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/ilgarcia"
+              className={styles.svgDisplay}
+              aria-label="GitHub"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiGithub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/ilgarcia/"
+              className={styles.svgDisplay}
+              aria-label="LinkedIn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="/resume.pdf"
+              className={styles.svgDisplay}
+              aria-label="Resume"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FiFileText />
+            </a>
+          </li>
+        </ul>
+      </m.div>
+      <div className={styles.containerContact}>
+        <Image
+          src={contact}
+          alt="Contact me"
+          className={styles.contactPic}
+          fill
+          sizes="100%"
+        />
+        <h2>CONTACT ME</h2>
+        <div className={styles.socialsLinks}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            veritatis quam rerum sunt officia culpa, commodi excepturi. Enim
+            nisi quisquam natus omnis cumque minima at, esse dignissimos eaque
+            ipsum beatae.
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/ilgarcia"
+                aria-label="GitHub"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FiGithub />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/ilgarcia/"
+                aria-label="LinkedIn"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FiLinkedin />
+              </a>
+            </li>
+            <li>
+              <a
+                href="/resume.pdf"
+                aria-label="Resume"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiFileText />
+              </a>
+            </li>
+          </ul>
+          <p>
+            <a
+              href="mailto:limagarcia.igor@gmail.com"
+              className={styles.emailDisplay}
+            >
+              limagarcia.igor@gmail.com
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
