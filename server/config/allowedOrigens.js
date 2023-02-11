38bbae7c6f0fd077693b3a682c004f24912bb6f4
ilgarcia <60369262+ilgarcia@users.noreply.github.com>
2023-02-11 20:34:58 -0300

    Correcting Cors options

--- server/config/allowedOrigens.js
+++ server/config/allowedOrigens.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-  "http://localhost:3000",
+  // "http://localhost:3000",
   "https://api.ilgarcia.com/",
+  "https://www.ilgarcia.com/",
 ];
 
 module.exports = allowedOrigins;
