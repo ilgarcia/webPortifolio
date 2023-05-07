66217262ef64ff7c58e266de76a614bea9bf3ee4
ilgarcia <60369262+ilgarcia@users.noreply.github.com>
2023-05-07 17:11:43 -0300

    updating dependencies

--- client/src/app/(user)/blog/page.tsx
+++ client/src/app/(user)/blog/page.tsx
@@ -19,22 +19,22 @@ const query = groq`
 `;
 
 export default async function Page() {
-  if (previewData()) {
-    return (
-      <section>
-        <BannerBlog />
-        <PreviewSuspense
-          fallback={
-            <div role="status">
-              <p>Loading Preview Data...</p>
-            </div>
-          }
-        >
-          <PreviewBlogList query={query} />
-        </PreviewSuspense>
-      </section>
-    );
-  }
+  // if (previewData()) {
+    // return (
+    //   <section>
+    //     <BannerBlog />
+    //     <PreviewSuspense
+    //       fallback={
+    //         <div role="status">
+    //           <p>Loading Preview Data...</p>
+    //         </div>
+    //       }
+    //     >
+    //       <PreviewBlogList query={query} />
+    //     </PreviewSuspense>
+    //   </section>
+    // );
+  // }
 
   const posts = await client.fetch(query);
 
